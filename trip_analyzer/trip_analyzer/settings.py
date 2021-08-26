fa977f2a0ee9220cb074d85e7943616b40e88f18
Ronald Fernando Rodríguez Barbosa <ronaldraxon@gmail.com>
2021-08-26 07:34:23 -0500

    including secret key for execution

--- trip_analyzer/trip_analyzer/settings.py
+++ trip_analyzer/trip_analyzer/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_APP_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_APP_SECRET_KEY') # secret_key: 'django-insecure-rdh^-7g9-8sg(xov3*y5v!&+6o2z4nv%4*6_6i9j86===atj3e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_APP_DEBUG')
